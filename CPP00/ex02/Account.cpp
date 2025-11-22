158049c185a852f18ceddc9957e186be870d897b
Nonojim <nolan.pederencino@gmail.com>
2025-11-22 16:46:13 +0100

    starting ex02

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 15:34:06 by npederen          #+#    #+#             */
+/*   Updated: 2025/11/22 16:41:15 by npederen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account :: Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_amount = initial_deposit;
+	std::cout << "[display timestamp]" << " index:" << _nbAccounts << ";amount:" << _amount << ";created" << std::endl;
+	_totalAmount += initial_deposit;
+	_nbAccounts++;
+}
+
+Account :: ~Account(void)
+{
+	std::cout << "[display timestamp]" << " index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+
+}
+
+void Account::displayAccountsInfos()
+{
+	std::cout << "[display timestamp]" << " accounts:" << _nbAccounts << ";total:"
+		<< _totalAmount << ";deposits:" << _totalNbDeposits << ":withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+void Account::displayStatus() const
+{
+	std::cout << "[display timestamp]" << " index:" << _accountIndex << ";amount:"
+		<< _amount << ";deposits:" << _nbDeposits << ":withdrawals:" << _nbWithdrawals << std::endl;
+}
